6dc8f6ab304e5c5771d9ab9e761144d0faa6835f
mansenfranzen <franz.woellert@gmail.com>
2017-11-19 13:29:43 +0100

    Automatically accept conda license agreement when installing. Provide installation path for conda for silent installation.

--- ubuntu/16.04LTS/setup_sci_stack.sh
+++ ubuntu/16.04LTS/setup_sci_stack.sh
@@ -8,8 +8,10 @@
 ########################
 
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh # (04.10.17)
-bash Miniconda3-latest-Linux-x86_64.sh # choose directory corresponding to CONDA_HOME
+bash Miniconda3-latest-Linux-x86_64.sh -b -p $CONDA_HOME # automatically accept license
 rm Miniconda3-latest-Linux-x86_64.sh # remove install file again
+
+echo 'export PATH="$CONDA_HOME/bin:$PATH"' 
 
 # add conda-forge
 source .bashrc # activate conda
